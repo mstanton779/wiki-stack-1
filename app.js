56a6c89695b359bc770bb82cb9447a81446c5642
Mike Stanton <mstanton779@gmail.com>
2019-11-14 11:45:43 -0600

    set up routes, set up models, models syncing to db, and probably some other shit. I also spit everywhere.

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-const express = require('express'); 
-const app = express(); 
-const morgan = require('morgan'); 
-const sequelize = require('sequelize'); 
-const layout = require('./views/layout');
-const { db } = require('./models');
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const sequelize = require('sequelize')
+const layout = require('./views/layout')
+const { db } = require('./models')
+const wikiRouter = require('./routes/wiki')
+const userRouter = require('./routes/user')
+// console.log(db)
+db.authenticate().then(() => {
+    console.log('connected to the database')
+})
 
-db.authenticate().
-then(() => {
-  console.log('connected to the database');
-});
-
-
-app.use(morgan('dev')); 
-app.use(express.static(__dirname + '/public')); 
+app.use(morgan('dev'))
+app.use(express.static(__dirname + '/public'))
 // parses url-encoded bodies
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }))
 // parses json bodies
 app.use(express.json())
 
-app.get('/', (req, res, next) => {
-    res.send(layout(" ")); 
-})
-
-app.listen(1337, () => {
-    console.log('listening'); 
-})
+app.use('/users', userRouter)
+app.use('/wiki', wikiRouter)
+const init = async () => {
+    // await Page.sync({ force: true })
+    // await User.sync({ force: true })
+    await db.sync({ force: true })
+    app.listen(1337, () => {
+        console.log('listening')
+    })
+}
+init()
--- models/index.js
+++ models/index.js
@@ -1,42 +1,46 @@
-const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack');
+const Sequelize = require('sequelize')
+const db = new Sequelize('postgres://localhost:5432/wikistack', {
+    logging: false,
+})
 
 const Page = db.define('pages', {
     title: {
-        type: Sequelize.STRING, 
-        allowNull: false
-    }, 
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        },
+    },
     content: {
         type: Sequelize.TEXT, //has unlimited length, as opposed to string
-        allowNull: false  
-    }, 
+        allowNull: false,
+    },
     status: {
-        type: Sequelize.ENUM('open', 'closed'), 
-        allowNull: false, 
-        defaultValue: 'closed'
-    }, 
-    slug:{
-        type: Sequelize.STRING, 
-        allowNull: false
-    }
-});
+        type: Sequelize.ENUM('open', 'closed'),
+        // allowNull: false,
+        defaultValue: 'closed',
+    },
+    slug: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+})
 
 const User = db.define('users', {
     name: {
-        type: Sequelize.STRING, 
-        allowNull: false
-    }, 
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+        },
+    },
     email: {
         type: Sequelize.STRING, //has unlimited length, as opposed to string
-        allowNull: false, 
+        allowNull: false,
         validate: {
-            isEmail: true
-        }  
-    }
-});
+            isEmail: true,
+        },
+    },
+})
 
-module.exports = {
-  db, 
-  Page, 
-  User
-}
+module.exports = { db }
